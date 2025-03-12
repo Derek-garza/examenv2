d5305aa655109959875d399a6722c5a6a30c76e0
DEREK_SANTIAGO_GARZA__ <a.22308051281196@cbtis128.edu.mx>
2025-03-12 18:41:14 +0000

    examen v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Producto {
+  final int codigo;
+  final String nombre;
+  final double peso;
+
+  Producto({required this.codigo, required this.nombre, required this.peso});
+}
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+              'Lista de productos\nDerek Garza M 1196'), // Agregamos el texto al AppBar
+          backgroundColor:
+              Color(0xff396f9f), // Cambiamos el color del AppBar a azul
+        ),
+        body: ListaProductos(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class ListaProductos extends StatelessWidget {
+  final List<Producto> productos = [
+    Producto(codigo: 1, nombre: 'guantes reyes rojo', peso: 0.2),
+    Producto(codigo: 2, nombre: 'bucal negro', peso: 0.15),
+    Producto(codigo: 3, nombre: 'cereta reyes roja', peso: 0.18),
+    Producto(codigo: 4, nombre: 'guantes everlast blanco', peso: 0.3),
+    Producto(codigo: 5, nombre: 'Pera de boxeo', peso: 0.22),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: productos.map((producto) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Card(
+                    color: _getColor(producto.codigo),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Código: ${producto.codigo}'),
+                          Text('Nombre: ${producto.nombre}'),
+                          Text('Peso: ${producto.peso} kg'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
+  }
+
+  Color _getColor(int codigo) {
+    switch (codigo) {
+      case 1:
+        return Color(0xffc53644)!;
+      case 2:
+        return Color(0xffdc8500)!;
+      case 3:
+        return Color(0xffd0bc00)!;
+      case 4:
+        return Color(0xff02d709)!;
+      case 5:
+        return Color(0xff2292ec)!;
+      default:
+        return Colors.grey[100]!;
+    }
   }
 }
